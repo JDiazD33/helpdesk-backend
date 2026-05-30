d703b1f6a9cf07e72de4042100b681dd1881da89
JDiazD <joaquinqg1@gmail.com>
2026-05-29 20:44:31 -0500

    Agregar métodos: buscarPorNombre, listarEmpresasRecientes, getRankingEmpresas

--- src/main/java/com/helpdesk/helpdesk_backend/service/EmpresaService.java
+++ src/main/java/com/helpdesk/helpdesk_backend/service/EmpresaService.java
@@ -1,19 +1,17 @@
 package com.helpdesk.helpdesk_backend.service;
 
 import java.util.List;
-
 import com.helpdesk.helpdesk_backend.dto.EmpresaRequestDTO;
 import com.helpdesk.helpdesk_backend.dto.EmpresaResponseDTO;
+import com.helpdesk.helpdesk_backend.dto.EmpresaTicketsDTO;
 
 public interface EmpresaService {
-
     EmpresaResponseDTO crearEmpresa(EmpresaRequestDTO requestDTO);
-    
     EmpresaResponseDTO obtenerEmpresaPorId(Long id);
-    
     List<EmpresaResponseDTO> listarEmpresasActivas();
-    
     EmpresaResponseDTO actualizarEmpresa(Long id, EmpresaRequestDTO requestDTO);
-    
     void eliminarEmpresa(Long id);
+    List<EmpresaResponseDTO> buscarPorNombre(String texto);
+    List<EmpresaResponseDTO> listarEmpresasRecientes(int dias);
+    List<EmpresaTicketsDTO> getRankingEmpresas();
 }

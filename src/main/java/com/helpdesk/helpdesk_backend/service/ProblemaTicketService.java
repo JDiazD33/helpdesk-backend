06e49ab81f15a8709fb9bb4a7918267eea2eaab0
JDiazD <joaquinqg1@gmail.com>
2026-05-29 20:45:17 -0500

    Agregar métodos contarProblemasPorCategoria y buscarPorTexto

--- src/main/java/com/helpdesk/helpdesk_backend/service/ProblemaTicketService.java
+++ src/main/java/com/helpdesk/helpdesk_backend/service/ProblemaTicketService.java
@@ -1,21 +1,17 @@
 package com.helpdesk.helpdesk_backend.service;
 
 import java.util.List;
-
+import java.util.Map;
 import com.helpdesk.helpdesk_backend.dto.ProblemaRequestDTO;
 import com.helpdesk.helpdesk_backend.dto.ProblemaResponseDTO;
 
 public interface ProblemaTicketService {
-
     List<ProblemaResponseDTO> listarProblemasPorCategoria(Long categoriaId, Long empresaId);
-
     List<ProblemaResponseDTO> listarActivosPorEmpresa(Long empresaId);
-
     ProblemaResponseDTO obtenerPorId(Long id, Long empresaId);
-
     ProblemaResponseDTO crearProblema(ProblemaRequestDTO requestDTO, Long empresaId);
-
     ProblemaResponseDTO actualizarProblema(Long id, ProblemaRequestDTO requestDTO, Long empresaId);
-
     void eliminarProblema(Long id, Long empresaId);
+    List<Map<String, Object>> contarProblemasPorCategoria();
+    List<ProblemaResponseDTO> buscarPorTexto(String texto);
 }

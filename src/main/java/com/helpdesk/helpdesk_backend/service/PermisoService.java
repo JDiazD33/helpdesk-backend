b93d99cde3241280338ad6b4e0b0840f781e41d3
JDiazD <joaquinqg1@gmail.com>
2026-05-29 20:44:53 -0500

    Agregar método buscarPorTexto

--- src/main/java/com/helpdesk/helpdesk_backend/service/PermisoService.java
+++ src/main/java/com/helpdesk/helpdesk_backend/service/PermisoService.java
@@ -2,20 +2,14 @@ package com.helpdesk.helpdesk_backend.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import com.helpdesk.helpdesk_backend.model.Permiso;
 
 public interface PermisoService {
-
     List<Permiso> listarTodos();
-
     List<Permiso> listarActivos();
-
     Optional<Permiso> buscarPorId(Long id);
-
     Permiso guardar(Permiso permiso);
-
     Permiso actualizar(Long id, Permiso permiso);
-
     void eliminar(Long id);
+    List<Permiso> buscarPorTexto(String texto);
 }
